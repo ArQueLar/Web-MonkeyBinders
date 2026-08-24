41ac74550f061d293f8d30b575804c23b6806536
Aaron Quevedo <buny01234@outlook.com>
2026-08-24 12:00:31 +0200

    arreglado filtro pedido

--- api/orders.js
+++ api/orders.js
@@ -27,7 +27,10 @@ export default async function handler(req, res) {
         const orders = await callOdoo(ODOO_URL, 'object', 'execute_kw', [
             ODOO_DB, uid, ODOO_API_KEY,
             'sale.order', 'search_read',
-            [[['partner_id', '=', session.partnerId]]],
+            [[
+                ['partner_id', '=', session.partnerId],
+                ['state', 'in', ['sale', 'done']] // solo pedidos confirmados de verdad, no presupuestos/borradores sin confirmar
+            ]],
             {
                 fields: ['name', 'date_order', 'amount_total', 'state'],
                 order: 'date_order desc',
